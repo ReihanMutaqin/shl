2911977a8d06918129fcb97609d583e32154ef2f
ReihanMutaqin <reyhanmutakin1@gmail.com>
2026-07-12 12:18:56 +0700

    fix: use Vercel native catch-all for TRPC API

--- api/[...route].ts
+++
@@ -1,9 +0,0 @@
-// Vercel serverless function entry point
-import { handle } from "hono/vercel";
-import app from "../server/boot.js";
-
-export default handle(app);
-
-export const config = {
-  runtime: "nodejs",
-};
